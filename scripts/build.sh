1d9bf8878e767690c4056a37cc5df6cdeafb9c5e
sugardon <sugar2don@gmail.com>
2022-07-12 22:12:30 +0900

    Add shipwright

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BUILD_DIR="build"
+mkdir -p BUILD_DIR
+rm $BUILD_DIR/*.yaml
+
+kustomize build argocd-apps/overlays/sugardon01 -o=$BUILD_DIR
+kustomize build pipelinesascode -o=$BUILD_DIR
+kustomize build setup -o=$BUILD_DIR
