79948a92c9f81e8dc5f56634c7779cf5f6e07705
sugardon <sugar2don@gmail.com>
2022-07-13 21:57:28 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,3 +7,4 @@ rm $BUILD_DIR/*.yaml
 kustomize build argocd-apps/overlays/sugardon01 -o=$BUILD_DIR
 kustomize build pipelinesascode -o=$BUILD_DIR
 kustomize build setup -o=$BUILD_DIR
+kustomize build tasks -o=$BUILD_DIR
