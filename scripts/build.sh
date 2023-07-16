c63004ada794c9360081a7606ef21e921c1fe2aa
sugardon <sugar2don@gmail.com>
2023-07-17 02:13:33 +0900

    Update kustomize

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,3 +14,8 @@ kustomize build tasks -o=$BUILD_DIR
 kustomize build triggertemplates -o=$BUILD_DIR
 kustomize build triggerbindings -o=$BUILD_DIR
 kustomize build configs -o=$BUILD_DIR
+# overlays
+kustomize build ingress/overlays/development -o=$BUILD_DIR
+kustomize build pipelines/overlays/development -o=$BUILD_DIR
+kustomize build rbac/overlays/development -o=$BUILD_DIR
+kustomize build storages/overlays/development -o=$BUILD_DIR
